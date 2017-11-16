dcc5f62f52e0c6d23c5c3c41ec36b3cb44c26239
Ubuntu <kamhonhoi@gmail.com>
2017-11-16 01:34:10 +0000

    Updated Virtual Environment

--- HybBCSeq-venv/bin/activate.fish
+++ HybBCSeq-venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ubuntu/HybBCSeq-venv"
+set -gx VIRTUAL_ENV "/home/ubuntu/HybBCSeq/HybBCSeq-venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
